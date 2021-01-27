6c0867f9dadffd1f14f54c87e4d1bf2d54d2bcd6
Joan López de la Franca Beltran <joanjan14@gmail.com>
2021-01-27 15:13:37 +0100

    Add lesson02 / video02

---
+++ 02-02-repository-injection/internal/platform/server/handler/courses/create.go
@@ -0,0 +1,33 @@
+package courses
+
+import (
+	"net/http"
+
+	mooc "github.com/CodelyTV/go-hexagonal_http_api-course/02-02-repository-injection/internal"
+	"github.com/gin-gonic/gin"
+)
+
+type createRequest struct {
+	ID       string `json:"id" binding:"required"`
+	Name     string `json:"name" binding:"required"`
+	Duration string `json:"duration" binding:"required"`
+}
+
+// CreateHandler returns an HTTP handler for courses creation.
+func CreateHandler(courseRepository mooc.CourseRepository) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req createRequest
+		if err := ctx.BindJSON(&req); err != nil {
+			ctx.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		course := mooc.NewCourse(req.ID, req.Name, req.Duration)
+		if err := courseRepository.Save(ctx, course); err != nil {
+			ctx.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		ctx.Status(http.StatusCreated)
+	}
+}
---
+++ 02-02-repository-injection/internal/platform/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"fmt"
+	"log"
+
+	mooc "github.com/CodelyTV/go-hexagonal_http_api-course/02-02-repository-injection/internal"
+	"github.com/CodelyTV/go-hexagonal_http_api-course/02-02-repository-injection/internal/platform/server/handler/courses"
+	"github.com/CodelyTV/go-hexagonal_http_api-course/02-02-repository-injection/internal/platform/server/handler/health"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	httpAddr string
+	engine   *gin.Engine
+
+	// deps
+	courseRepository mooc.CourseRepository
+}
+
+func New(host string, port uint, courseRepository mooc.CourseRepository) Server {
+	srv := Server{
+		engine:   gin.New(),
+		httpAddr: fmt.Sprintf("%s:%d", host, port),
+
+		courseRepository: courseRepository,
+	}
+
+	srv.registerRoutes()
+	return srv
+}
+
+func (s *Server) Run() error {
+	log.Println("Server running on", s.httpAddr)
+	return s.engine.Run(s.httpAddr)
+}
+
+func (s *Server) registerRoutes() {
+	s.engine.GET("/health", health.CheckHandler())
+	s.engine.POST("/courses", courses.CreateHandler(s.courseRepository))
+}
