55c9c7fca65a55265023f62b80a20e0faf8b82b1
Joan López de la Franca Beltran <joanjan14@gmail.com>
2021-01-27 12:07:19 +0100

    Add MySQL container + sql table schema

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE courses
+(
+    id       VARCHAR(255) NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    duration VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id)
+
+) CHARACTER SET utf8mb4
+  COLLATE utf8mb4_bin;
