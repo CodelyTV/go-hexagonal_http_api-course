894683d5f22df40e47772603ed02999de6a49096
Rafael Calleja <rafaelcalleja@gmail.com>
2022-01-11 09:33:15 +0100

    fix pull events

---
+++ 07-03-event-bus-implementation/internal/course_test.go
@@ -0,0 +1,16 @@
+package mooc
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestCourse_PullEvents(t *testing.T) {
+	courseID, courseName, courseDuration := "37a0f027-15e6-47cc-a5d2-64183281087e", "Test Course", "10 months"
+	course, err := NewCourse(courseID, courseName, courseDuration)
+	require.NoError(t, err)
+
+	assert.Len(t, course.PullEvents(), 1)
+	assert.Len(t, course.PullEvents(), 0)
+}
